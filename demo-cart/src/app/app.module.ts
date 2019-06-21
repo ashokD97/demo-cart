cdb11d5a0e33189d4225945448388a3622bd4bb2
moh-mustakeem <mustakeem0917@gmail.com>
2019-06-21 14:55:51 +0530

    cart order and products components created

--- demo-cart/src/app/app.module.ts
+++ demo-cart/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ProductsComponent } from './pos/products/products.component';
+import { CartComponent } from './pos/cart/cart.component';
+import { OrderComponent } from './pos/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CartComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
