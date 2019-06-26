7fdbc43287391d813cee25be1b57c746db1b07c8
moh-mustakeem <mustakeem0917@gmail.com>
2019-06-26 12:56:54 +0530

    cart service addFunction

--- demo-cart/src/app/pos/cart/cart.component.ts
+++ demo-cart/src/app/pos/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductListService } from '../product-list-service/product-list.service';
 
 @Component({
   selector: 'cart',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private productListService :ProductListService) { }
+  cartItems:any;
   ngOnInit() {
+    this.productListService.getCart().subscribe(res=>{
+      //  console.log(res);
+    });
   }
 
+  addToCart(item){
+    this.productListService.addToCart(item);
+  }
 }
--- demo-cart/src/app/pos/product-list-service/product-list.service.ts
+++ demo-cart/src/app/pos/product-list-service/product-list.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable,Subject } from 'rxjs';
+
+interface cartItem{
+  name:string,
+  price:number,
+  quantity:number,
+  total:number
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +15,32 @@ import { Observable } from 'rxjs';
 export class ProductListService {
 
   constructor(private http :HttpClient) { }
-  
+  private cartItems = new Subject<any>();
+  currentCart = this.cartItems.asObservable();
+
   getAllProducts(): Observable<any>{
     const _url = "assets/pos.products.json";
     return this.http.get(_url);
+  }
+  
+  addToCart(item){
+    this.getCart().subscribe(res=>{
+      let _cart = res;
+      res.forEach(element => {
+        if(element['name'] == item['name']){
+           _cart[element]['quantity']++;
+           _cart[element]['total'] = _cart[element]['price']*_cart[element]['quantity'];
+        }else{
+          item['quantity'] = 1;
+          item['total'] = item['price'];
+          _cart.push(item);
+        }
+      });
+      this.cartItems.next(_cart);
+    });
+  }
+
+  getCart(){
+    return this.currentCart;
   }
 }
